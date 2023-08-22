5f410f3084376b22ba1f39b3c7badf189508299d
rtmzk <13581854511@163.com>
2023-08-22 15:31:39 +0800

    fix: error message fixed

--- internal/errs/error.go
+++ internal/errs/error.go
@@ -28,9 +28,9 @@ func NewErrInvalidType(want, got string) error {
 }
 
 func NewErrInvalidIntervalValue(interval time.Duration) error {
-	return fmt.Errorf("toolkit: 无效时间间隔 %d,预期值大于 0", interval)
+	return fmt.Errorf("toolkit: 无效的间隔时间 %d, 预期值应大于 0", interval)
 }
 
 func NewErrInvalidMaxIntervalValue(maxInterval, initialInterval time.Duration) error {
-	return fmt.Errorf("toolkit: 最大重试间隔时间 [%d] 应大于初始重试间隔时间 [%d]", maxInterval, initialInterval)
+	return fmt.Errorf("toolkit: 最大重试间隔的时间 [%d] 应大于等于初始重试的间隔时间 [%d] ", maxInterval, initialInterval)
 }
